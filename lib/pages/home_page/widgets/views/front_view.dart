6a1e92d2d127e1f3485b5961ee359b81325e81bd
mittalayush1430 <mittalayush1430@gmail.com>
2021-06-02 23:29:34 +0530

    added assets

---
+++ lib/pages/home_page/widgets/views/front_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FrontView extends StatelessWidget {
+  const FrontView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
